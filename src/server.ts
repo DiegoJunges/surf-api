366e10834c45bb000ffe34fa8b8e5f7d98abbb76
Diego Junges <diegojunges@hotmail.com>
2021-02-07 00:45:44 -0300

    adding beachesController

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
 
 export class SetupServer extends Server {
   constructor(private port = 3333) {
@@ -23,7 +24,8 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
 
